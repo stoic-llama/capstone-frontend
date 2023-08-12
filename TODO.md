654182a12b5a5c2e5e3b37bcad7963c3ce097b88
John Liu <stoic.llama@gmail.com>
2023-08-12 14:04:12 -0400

    Added formattedDateNow() to healthcheck timestamp

--- TODO.md
+++ TODO.md
@@ -1,7 +1,10 @@
-## Updated 7/30/2023
+## Updated 8/10/2023
 
-1. Finish Log Out page
-2. Finish Password Reset page
+~~1. Finish Log Out page~~
+
+~~2. Finish Password Reset page~~
+
 3. Finish Like / Dislike logic (Frontend, Backend)
+
 4. **Stretch Goal** Finish the web scraping logic for Target website and loading into database
 
--- serverMiddleware/healthcheck.js
+++ serverMiddleware/healthcheck.js
@@ -6,7 +6,7 @@ export default {
             res.end(JSON.stringify({
                 message: 'OK',
                 uptime: Math.floor(process.uptime()) + " seconds",
-                timestamp: new Date(Date.now()).toString() 
+                timestamp: formattedDateNow() // new Date(Date.now()).toString() 
             }));
         } catch (error) {
             res.setHeader('Content-Type', 'application/json');
@@ -14,9 +14,33 @@ export default {
             res.end(JSON.stringify({
                 message: error.message,
                 uptime: Math.floor(process.uptime()) + " seconds",
-                timestamp: new Date(Date.now()).toString() 
+                timestamp: formattedDateNow() // new Date(Date.now()).toString() 
             }));
         }
     },
     path: '/healthcheck'
-  }
+}
+
+
+function formattedDateNow() {
+   var result = "" 
+   var d = new Date(Date.now())
+
+   // format ---> 'YYYY/MM/D hh:mm:ss SSS'
+
+   result = result + d.getFullYear()
+            + "/"
+            + (d.getMonth()+1)
+            + "/"
+            + d.getDate().toString().padStart(2,0) 
+            + " "
+            + d.getHours().toString().padStart(2,0)
+            + ":"
+            + d.getMinutes().toString().padStart(2,0)
+            + ":"
+            + d.getSeconds().toString().padStart(2,0)
+            + "."
+            + d.getMilliseconds().toString().padStart(3,0)
+
+   return result;
+}
