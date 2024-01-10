cb2fb851ff9aa9a780174382fcd936f280440a5c
John Liu <stoic.llama@gmail.com>
2024-01-10 10:54:36 -0500

    Changed time format

--- serverMiddleware/healthcheck.js
+++ serverMiddleware/healthcheck.js
@@ -31,9 +31,9 @@ function formattedDateNow() {
    // format ---> 'YYYY/MM/D hh:mm:ss SSS'
 
    result = result + d.getFullYear()
-            + "/"
+            + "-"
             + String(d.getMonth() + 1).padStart(2, '0') // (d.getMonth()+1)
-            + "/"
+            + "-"
             + d.getDate().toString().padStart(2,0) 
             + " "
             + d.getHours().toString().padStart(2,0)
@@ -41,8 +41,8 @@ function formattedDateNow() {
             + d.getMinutes().toString().padStart(2,0)
             + ":"
             + d.getSeconds().toString().padStart(2,0)
-            + "."
-            + d.getMilliseconds().toString().padStart(2,0)
+            // + "."
+            // + d.getMilliseconds().toString().padStart(2,0)
 
    return result;
 }
